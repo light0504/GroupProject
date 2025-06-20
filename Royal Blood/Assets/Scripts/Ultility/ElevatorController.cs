00e25e2a60a8636d4481896233a211f0aeec451f
light0504 <lightlan0504@gmail.com>
2025-06-20 18:04:29 +0700

    Add EndGame and Dialogue

--- Royal Blood/Assets/Scripts/Ultility/ElevatorController.cs
+++ Royal Blood/Assets/Scripts/Ultility/ElevatorController.cs
@@ -89,7 +89,6 @@ public class ElevatorController : MonoBehaviour
         if (other.CompareTag("Player") && !isMoving && !isPlayerOnboard)
         {
             isPlayerOnboard = true;
-            // Tự động di chuyển đến điểm còn lại
             Transform destination = (currentTarget == destination_A) ? destination_B : destination_A;
             CallTo(destination);
         }
@@ -109,6 +108,7 @@ public class ElevatorController : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.transform.SetParent(null);
+            DontDestroyOnLoad(collision.gameObject);
             isPlayerOnboard = false;
             UpdateCallButtons();
         }
---
+++ Royal Blood/Assets/Scripts/Ultility/TextBlock.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class TextBlock : MonoBehaviour
+{
+
+    [Header("Hiển thị Thông tin Portal")]
+    public string textDisplayed;
+    public TextMeshProUGUI destinationText;
+    public bool canReview;
+    public float trackRadius;
+
+    private CircleCollider2D infoCollider;
+
+    private void Awake()
+    {
+        infoCollider = GetComponent<CircleCollider2D>();
+        infoCollider.radius = trackRadius;
+        infoCollider.isTrigger = true;
+
+        if (destinationText != null)
+        {
+            destinationText.text = textDisplayed;
+            destinationText.gameObject.SetActive(false);
+        }
+    }
+
+    // --- Xử lý va chạm ---
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.IsTouching(infoCollider))
+        {
+            if (destinationText != null) 
+            {
+                destinationText.gameObject.SetActive(true); 
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && destinationText != null)
+        {
+            if (!canReview)
+            {
+                Destroy(this.gameObject);
+            }
+            
+            destinationText.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, trackRadius);
+    }
+
+}
